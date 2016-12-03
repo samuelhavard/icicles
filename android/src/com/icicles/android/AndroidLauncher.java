6e2daf33829631b265c66b2316f05d79abac59d4
Samuel Havard <SamuelHavard@yahoo.com>
2016-12-03 15:36:07 -0500

    Initial commit

---
+++ android/src/com/icicles/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.icicles.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.icicles.IciclesGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new IciclesGame(), config);
+	}
+}
---
+++ html/src/com/icicles/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.icicles.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.icicles.IciclesGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new IciclesGame();
+        }
+}
