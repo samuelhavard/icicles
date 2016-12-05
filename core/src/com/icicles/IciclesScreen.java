4ba58e56d5cd84ab7fb64962d2d9eba5ca24f91f
Samuel Havard <SamuelHavard@yahoo.com>
2016-12-05 07:25:22 -0500

    Implement methods in IciclesScreen

--- core/src/com/icicles/IciclesScreen.java
+++ core/src/com/icicles/IciclesScreen.java
@@ -1,6 +1,9 @@
 package com.icicles;
 
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
 
 /**
  * Created by samue_000 on 12/3/2016.
@@ -9,10 +12,16 @@ import com.badlogic.gdx.Screen;
 public class IciclesScreen implements Screen {
 
     public static final  String TAG = IciclesScreen.class.getName();
+    ExtendViewport iciclesViewport;
+    ShapeRenderer renderer;
+    Icicle icicle;
 
     @Override
     public void show() {
-
+        iciclesViewport = new ExtendViewport(Constants.WORLD_SIZE, Constants.WORLD_SIZE);
+        renderer = new ShapeRenderer();
+        renderer.setAutoShapeType(true);
+        icicle = new Icicle(new Vector2(Constants.WORLD_SIZE / 2, Constants.WORLD_SIZE / 2));
     }
 
     @Override
