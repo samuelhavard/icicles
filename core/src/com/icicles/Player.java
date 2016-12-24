49c35afd1803cd37a51fd98f83b81bc6f59c6112
samuelhavard <SamuelHavard@yahoo.com>
2016-12-24 11:28:42 -0500

    update player class

--- core/src/com/icicles/Player.java
+++ core/src/com/icicles/Player.java
@@ -7,4 +7,24 @@ package com.icicles;
 public class Player {
     public static final String TAG = Player.class.getName();
 
+    Vector2 position;
+
+    Viewport viewport;
+
+    public Player(Viewport viewport) {
+        this.viewport = viewport;
+        init();
+    }
+
+    public void init() {
+        position = new Vector2(viewport.getWorldWidth / 2, Constants.PLAYER_HEAD_HEIGHT);
+    }
+
+    public void Renderer(ShapeRenderer renderer) {
+        renderer.setColor(Constants.PLAYER_COLOR);
+    }
+
+
+
+
 }
